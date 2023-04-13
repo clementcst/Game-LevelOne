1178f0ac9c1ee6ad924acab1e7389509b71de33c
Clément <82080806+clementcst@users.noreply.github.com>
2023-04-13 10:00:18 +0200

    Add files via upload

---
+++ LEVELONE/src/game/Game.java
@@ -0,0 +1,92 @@
+package game;
+
+
+
+import game.map.Map;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.input.KeyCode;
+import javafx.stage.Stage;
+
+
+
+public class Game extends Application {
+
+    private static final int SCENE_WIDTH = 1216;
+    private static final int SCENE_HEIGHT = 704;
+    private int timeSeconds = 10; // Le temps en secondes
+
+    
+    //private Player player;
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        
+    	Map map = new Map(SCENE_WIDTH, SCENE_HEIGHT);
+        Scene scene = map.createMap();
+        
+
+        scene.setOnKeyPressed(event -> {
+            KeyCode keyCode = event.getCode();
+            switch (keyCode) {
+                case Z:
+                	for(int i = 0; i < 10; i++) {
+                		map.getPlayer().getSprite().setY(map.getPlayer().getSprite().getY() - 1);
+                    	if(map.getPlayer().testCollision(map.getGridpaneObstacle())) {
+                    		map.getPlayer().getSprite().setY(map.getPlayer().getSprite().getY() + 1);
+                    	}
+                    	if(map.getPlayer().testCollision(map.getGridpaneInteract())) {
+                    		System.out.println("AIE LE PERSONNAGE DE STAN M'A FAIT MAL !");
+                    	}
+                	}
+                	
+                    break;
+                case S:
+                	for(int i = 0; i < 10; i++) {
+	                	map.getPlayer().getSprite().setY(map.getPlayer().getSprite().getY() + 1);
+	                	if(map.getPlayer().testCollision(map.getGridpaneObstacle())) {
+	                		map.getPlayer().getSprite().setY(map.getPlayer().getSprite().getY() - 1);
+	                	}
+	                	if(map.getPlayer().testCollision(map.getGridpaneInteract())) {
+	                		System.out.println("AIE LE PERSONNAGE DE STAN M'A FAIT MAL !");
+	                	}
+                	}
+                    break;
+                case Q:
+                	for(int i = 0; i < 10; i++) {
+	                	map.getPlayer().getSprite().setX(map.getPlayer().getSprite().getX() - 1);
+	                	if(map.getPlayer().testCollision(map.getGridpaneObstacle())) {
+	                		map.getPlayer().getSprite().setX(map.getPlayer().getSprite().getX() + 1);
+	                	}
+	                	if(map.getPlayer().testCollision(map.getGridpaneInteract())) {
+	                		System.out.println("AIE LE PERSONNAGE DE STAN M'A FAIT MAL !");
+	                	}
+                	}
+                    break;
+                case D:
+                	for(int i = 0; i < 10; i++) {
+	                	map.getPlayer().getSprite().setX(map.getPlayer().getSprite().getX() + 1);
+	                	if(map.getPlayer().testCollision(map.getGridpaneObstacle())) {
+	                		map.getPlayer().getSprite().setX(map.getPlayer().getSprite().getX() - 1);
+	                	}
+	                	if(map.getPlayer().testCollision(map.getGridpaneInteract())) {
+	                		System.out.println("AIE LE PERSONNAGE DE STAN M'A FAIT MAL !");
+	                	}
+                	}
+                    break;
+                default:
+                    break;
+            }
+            //System.out.println("X :" + map.getPlayer().getSprite().getX() + "Y :" + map.getPlayer().getSprite().getY());
+        });
+
+        primaryStage.setTitle("LEVELONE");
+        primaryStage.setResizable(false);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+    	launch(args);
+    }
+}
---
+++ LEVELONE/src/game/textures/Constants.java
@@ -0,0 +1,36 @@
+package game.textures;
+
+public class Constants {
+	//Initialisation des textures :
+			public static final Texture sol = new Texture("res/images/ground1.png");
+			public static final Texture black = new Texture("res/images/black.png");
+			
+			//bordure exterieur
+			public static final Texture topBorder = new Texture("res/images/topBorder.png");
+			public static final Texture leftBorder = new Texture("res/images/leftBorder.png");
+			public static final Texture rightBorder = new Texture("res/images/rightBorder.png");
+			public static final Texture bottomBorder = new Texture("res/images/bottomBorder.png");
+			public static final Texture bottomLeftCorner = new Texture("res/images/bottomLeftCorner.png");
+			public static final Texture bottomRightCorner = new Texture("res/images/bottomRightCorner.png");
+			public static final Texture topRightCorner = new Texture("res/images/topRightCorner.png");
+			public static final Texture topLeftCorner = new Texture("res/images/topLeftCorner.png");
+			
+			//Line et block int�rieur
+			public static final Texture topVerticalLine = new Texture("res/images/topVerticalLine.png");
+			public static final Texture verticalLine = new Texture("res/images/verticalLine.png");
+			public static final Texture bottomVerticalLine = new Texture("res/images/bottomVerticalLine.png");
+			public static final Texture leftHorizontalLine = new Texture("res/images/leftHorizontalLine.png");
+			public static final Texture horizontalLine = new Texture("res/images/horizontalLine.png");
+			//Texture rightHorizontalLine = new Texture("res/images/rightHorizontalLine.png");
+			
+			public static final Texture cornerLeft = new Texture("res/images/cornerLeft.png");
+			public static final Texture cornerRight = new Texture("res/images/cornerRight.png");
+			public static final Texture cornerRightBottom = new Texture("res/images/cornerRightBottom.png");
+			public static final Texture cornerLeftBottom = new Texture("res/images/cornerLeftBottom.png");
+			
+			public static final Texture block = new Texture("res/images/block.png");
+
+			
+			public static final Texture stan = new Texture("res/images/pigKing.png");
+			public static final Texture stan2 = new Texture("res/images/pigMob.png");
+}
