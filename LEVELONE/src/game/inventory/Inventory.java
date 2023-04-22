82e82861be26a33d30bd143fb190cb71cf56d67b
Adamou02 <42783707+Adamou02@users.noreply.github.com>
2023-04-22 19:10:30 +0200

    Update Inventory.java

--- LEVELONE/src/game/inventory/Inventory.java
+++ LEVELONE/src/game/inventory/Inventory.java
@@ -1,14 +1,12 @@
 package game.inventory;
 
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-
-import game.item.Item;
-import game.item.Potion;
+import game.item.AbstractItem;
 import game.item.Weapon;
+import game.textures.Constants;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.EventHandler;
+import javafx.geometry.Insets;
 /*import javafx.collections.FXCollections;*/
 import javafx.scene.Scene;
 import javafx.scene.control.ListView;
@@ -17,8 +15,12 @@ import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.*;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
+import javafx.scene.layout.Region;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
 
@@ -32,41 +34,120 @@ public class Inventory {
 	private ImageView centre;
 	
 	//instances de l'inventaire pour la gestion des items
-	protected Item[] item;
+	protected AbstractItem[] items;
 	protected int nbItem;
 	
 	
 	public Inventory() {	
 		//constructeur partie items
 		this.nbItem = 0;
-		this.item = new Item[6];
+		this.items = new AbstractItem[12];
 		
-		for(int i = 0 ; i < 6; i++) {
-			this.item[i] =(Item) new Weapon("");
+		for(int i = 0 ; i < 12; i++) {
+			this.items[i] = null;
 		}
 		
-		//constructeur partie graphique
-		/*this.album = new Album(this);*/
+		
+        //gridPane.setFillHeight(gridPane, null);
+        //for()
+        /*ImageView Adam = new ImageView(new Image(Constants.topBorder.getImagePath()));
+        Adam.setFitHeight(50);
+        Adam.setFitWidth(50);
+        gridPane.add(Adam, 0, 0);*/
+        
+        /*for (int col = 0; col < 4; col++) {
+    		for (int row = 0; row < 3; row++) {
+    			Region region = new Region();
+    			region.setPrefSize(50, 50);
+    			region.setMinSize(50,50);
+    			region.setMaxSize(50,50);
+    			region.setStyle("-fx-border-width: 1px; -fx-border-color: black;");
+    			gridPane.add(region, col, row);
+    		}
+		}*/
 		this.borderPane = new BorderPane();
 		this.borderPane.setMaxSize(600, 400);
-		this.borderPane.setCenter(new Pane(creerCentre("manchots")));
+		this.updateBorderPane();
 		
 		
 		this.stage = new Stage();
-		this.stage.setTitle("Album photo");
+		this.stage.setTitle("Inventaire");
 		this.stage.setScene(new Scene(this.borderPane));
 		this.stage.setResizable(false);
 		this.stage.sizeToScene();
 		this.stage.initModality(Modality.APPLICATION_MODAL);
 	}
 	
+	public void updateBorderPane() {
+		VBox infoPlayer = new VBox();
+		infoPlayer.setPrefHeight(50);
+		infoPlayer.setPrefWidth(500);
+		infoPlayer.setStyle("-fx-border-width: 1px; -fx-border-color: red;");
+		
+		
+		//Créez la grille d'items
+        GridPane gridPane = new GridPane();
+        gridPane.setPadding(new Insets(10));
+        gridPane.setHgap(10);
+        gridPane.setVgap(10);
+        
+        System.out.println(this.nbItem);
+        
+        for (int col = 0; col < 4; col++) {
+    		for (int row = 0; row < 3; row++) {
+    			Region region = new Region();
+    			region.setPrefSize(50, 50);
+    			region.setMinSize(50,50);
+    			region.setMaxSize(50,50);
+    			region.setStyle("-fx-border-width: 1px; -fx-border-color: black;");
+    			gridPane.add(region, col, row);
+    		}
+		}
+        
+        int col = 0;
+		int row = 0;
+		int compteur = 0;
+        for(AbstractItem item : items) {
+        	if (item != null) {
+	        	ImageView item_view = item.getImageView();
+	        	item_view.setFitHeight(50);
+	        	item_view.setFitWidth(50);
+	        	item_view.setStyle("-fx-border-width: 1px; -fx-border-color: black;");
+	        	if(compteur == 0) gridPane.add(item_view, 0, 0);
+	        	else {
+	        		gridPane.add(item_view, compteur%4, row);
+	        	}
+	        	compteur++;
+	        	
+	        	if(compteur%4 == 0) {
+	        		row++;
+	        	}
+        	}
+        }
+        
+        VBox ItemInfo = new VBox();
+        Text nom_item = new Text("Bonjour, comment allez-vous ?");
+        ItemInfo.getChildren().add(nom_item);
+        ItemInfo.getChildren().add(new ImageView(new Image(Constants.topBorder.getImagePath())));
+        Text description_item = new Text("Desc");
+        ItemInfo.getChildren().add(description_item);
+        ItemInfo.setPrefSize(300, 200);
+        ItemInfo.setStyle("-fx-border-width: 1px; -fx-border-color: blue;");
+
+		
+		//constructeur partie graphique
+		this.borderPane.setTop(infoPlayer);
+		this.borderPane.setCenter(gridPane);
+		this.borderPane.setRight(ItemInfo);
+	}
+	
 	//getters setters partie items 
-	public Item[] getItem() {
-		return item;
+	public AbstractItem[] getItem() {
+		return items;
 	}
 
-	public void setItem(Item[] item) {
-		this.item = item;
+	public void setItem(AbstractItem[] item) {
+		this.items = item;
 	}
 
 	public int getNbItem() {
@@ -77,16 +158,6 @@ public class Inventory {
 		this.nbItem = nbItem;
 	}
 
-	/*
-	//getters setter partie graphique
-	public Album getAlbum() {
-		return album;
-	}*/
-
-	/*
-	public void setAlbum(Album album) {
-		this.album = album;
-	}*/
 
 	public ImageView getCentre() {
 		return centre;
@@ -108,10 +179,10 @@ public class Inventory {
 		return borderPane;
 	}
 	
-	public void updateBorderPane() {
+	/*public void updateBorderPane() {
 		String [] name_items = new String[6];
 		for(int i = 0 ; i < 6; i++) {
-			name_items[i] = this.item[i].getName();
+			name_items[i] = this.items[i].getName();
 		}
 		
 		ObservableList<String> items = FXCollections.observableArrayList();
@@ -128,24 +199,13 @@ public class Inventory {
 		        //System.out.println(selectedItem);
 		    }
 		});
-		/*
-		liste.getSelectionModel().selectedItemProperty().addListener((observable, oldValue, newValue) -> {
-		    /*Item selectedItem = item.get(liste.getSelectionModel().getSelectedIndex());
-		    String imagePath = selectedItem.getPath();
 
-		    try {
-		        Image image = new Image(new FileInputStream(imagePath));
-		        ImageView imageView = new ImageView(image);
-		        borderPane.setCenter(imageView);
-		    } catch (FileNotFoundException e) {
-		        e.printStackTrace();
-		    }
-			System.out.println(liste);
-		});*/
 		
 		this.borderPane.setLeft(liste);
 		//this.borderPane.setCenter(new Pane(creerCentre()));
-	}
+	}*/
+	
+	
 
 
 	public HBox creerCentre(String imageName) {
@@ -156,31 +216,11 @@ public class Inventory {
 		hb.setPrefSize(600, 450);
 		return hb;
 	}
-	
-	/*
-	private ListView<String> creerListe() {
 
-		String[] noms = new String[this.nbItem];
-		for (int i = 0; i < this.nbItem; i++) {
-			noms[i] = album.getPhoto(i).getNom();
-		}
-		
-		ListView<String> liste = new ListView<>(FXCollections.observableArrayList(noms));
-		liste.getSelectionModel().select(album.getIndexCourant());
-		liste.getSelectionModel().setSelectionMode(SelectionMode.SINGLE);
-		liste.getSelectionModel().selectedItemProperty().addListener(ov -> {
-			album.setIndexCourant(liste.getSelectionModel().getSelectedIndex());
-			this.centre.setImage(album.getPhotoCourante().getImage());
-			this.centre.setFitWidth(400);
-			this.centre.setFitHeight(400);
-		});
-		return liste;
-	}*/
-	
-	
-	public void push(Item item) {
-		this.item[this.nbItem] = item;
+	public void push(AbstractItem item) {
+		this.items[this.nbItem] = item;
 		this.nbItem++;
+		System.out.println("on ajoute dans l'inventaire");
 		
 		/*ListView<String> liste = creerListe();
 		liste.setPrefWidth(150);
@@ -192,8 +232,11 @@ public class Inventory {
 	public void afficheInventory() {
 		System.out.println("Voici le contenu de l'inventaire :");
 		for(int i = 0 ; i < this.nbItem; i++) {
-			System.out.println(item[i].getName());
+			System.out.println(items[i].getName());
 		}
 	}
 	
+	public void setBorderPane(BorderPane borderPane) {
+		this.borderPane = borderPane;
+	}
 }
