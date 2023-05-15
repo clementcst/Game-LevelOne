7b8e517ca31758a434af87f277160c5cd73dd035
Adamou02 <42783707+Adamou02@users.noreply.github.com>
2023-05-15 18:45:26 +0200

    Update Minuteur.java

--- src/utils/Minuteur.java
+++ src/utils/Minuteur.java
@@ -1,6 +1,7 @@
 package utils;
 
 
+import game.popUp.ActionEndGame;
 import javafx.animation.Animation;
 import javafx.animation.KeyFrame;
 import javafx.animation.Timeline;
@@ -19,16 +20,16 @@ public class Minuteur extends GridPane {
     private Label timeLabel;
 
     public Minuteur() {
-        // Créer un label pour afficher le temps
+        // CrÃ©er un label pour afficher le temps
         timeLabel = new Label();
         timeLabel.setFont(new Font("Arial", 24));
-        timeLabel.setTextFill(Paint.valueOf("#FFF")); // définit la couleur du texte en rouge
+        timeLabel.setTextFill(Paint.valueOf("#FFF")); // dÃ©finit la couleur du texte en rouge
 
         timeLabel.setAlignment(Pos.CENTER);
         timeLabel.setPrefSize(64,32);
         //timeLabel.setStyle("-fx-color:white;");
 
-        // Ajouter le label à la GridPane
+        // Ajouter le label Ã  la GridPane
         this.add(timeLabel, 0, 0);
 
         // Configurer la GridPane
@@ -36,7 +37,7 @@ public class Minuteur extends GridPane {
         this.setPadding(new Insets(10));
         this.setHgap(10);
 
-        // Créer la timeline pour le minuteur
+        // CrÃ©er la timeline pour le minuteur
         timeline = new Timeline();
         timeline.setCycleCount(Animation.INDEFINITE);
         
@@ -45,25 +46,26 @@ public class Minuteur extends GridPane {
         timeline.getKeyFrames().add(new KeyFrame(Duration.seconds(1), new EventHandler<ActionEvent>() {
             @Override
             public void handle(ActionEvent event) {
-                // Mettre à jour le label avec le temps restant
+                // Mettre Ã  jour le label avec le temps restant
             	
                 int remainingTime = Integer.parseInt(timeLabel.getText()) - 1;
                 timeLabel.setText(Integer.toString(remainingTime));
                 //System.out.println(remainingTime);
-                // Arrêter le minuteur si le temps est écoulé
+                // ArrÃªter le minuteur si le temps est Ã©coulÃ©
                 if (remainingTime == 0) {
                     timeline.stop();
-                    timeLabel.setText("Temps écoulé !");
+                    timeLabel.setText("Temps Ã©coulÃ© !");
+                    ActionEndGame.displayEndGame(false);
                 }
             }
         }));
     }
 
     public void start(int durationInSeconds) {
-        // Définir la durée du minuteur en secondes
+        // DÃ©finir la durÃ©e du minuteur en secondes
         timeLabel.setText(Integer.toString(durationInSeconds));
 
-        // Démarrer la timeline
+        // DÃ©marrer la timeline
         timeline.playFromStart();
     }
 }
